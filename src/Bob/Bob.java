f7a556f4cf9ef7ce5251120705876cd09c8801dd
Michał Sokół <sokol.michal95@live.com>
2016-04-05 21:47:42 +0200

    Added SignedNote class

--- src/Bob/Bob.java
+++ src/Bob/Bob.java
@@ -1,11 +1,9 @@
 package Bob;
 
-import java.io.IOException;
-
 public class Bob {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        // TODO Auto-generated method stub
+    public static void main(String[] args) {
+        
         
     }
 
